e24a6a1496fba79055e9fbb5f7a273785d9af207
Cekap Yakup <61888196+cekapykp@users.noreply.github.com>
2022-08-13 17:16:39 +0300

    unknow

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -4,7 +4,6 @@ function Header() {
     return (
         <>
             <header>
-
                 <nav className="navbar navbar-expand-lg shadow-md py-2 bg-white relative flex items-center w-full justify-between">
                     <div className="px-6 w-full flex flex-wrap items-center justify-between">
                         <div className="flex items-center">
@@ -35,17 +34,20 @@ function Header() {
                         </div>
                         <div className="navbar-collapse collapse grow items-center" id="navbarSupportedContentY">
                             <ul className="navbar-nav mr-auto lg:flex lg:flex-row">
-                                <li className="nav-item">
-                                    <a className="nav-link block pr-2 lg:px-2 py-2 text-gray-600 hover:text-gray-700 focus:text-gray-700 transition duration-150 ease-in-out" href="#!" data-mdb-ripple="true" data-mdb-ripple-color="light">Home</a>
+                                <li className='nav-item py-1 lg:px-2'>
+                                    <img src="../../images/Starbucks_Corporation.png" width="70px" alt="" />
                                 </li>
                                 <li className="nav-item">
-                                    <a className="nav-link block pr-2 lg:px-2 py-2 text-gray-600 hover:text-gray-700 focus:text-gray-700 transition duration-150 ease-in-out" href="#!" data-mdb-ripple="true" data-mdb-ripple-color="light">Features</a>
+                                    <a className="nav-link block pr-2 lg:px-2 lg:py-5 py-2 text-gray-600 hover:text-gray-700 focus:text-gray-700 transition duration-150 ease-in-out" href="#!" data-mdb-ripple="true" data-mdb-ripple-color="light">STARBUCKS REWARD</a>
                                 </li>
                                 <li className="nav-item">
-                                    <a className="nav-link block pr-2 lg:px-2 py-2 text-gray-600 hover:text-gray-700 focus:text-gray-700 transition duration-150 ease-in-out" href="#!" data-mdb-ripple="true" data-mdb-ripple-color="light">Pricing</a>
+                                    <a className="nav-link block pr-2 lg:px-2 lg:py-5 py-2 text-gray-600 hover:text-gray-700 focus:text-gray-700 transition duration-150 ease-in-out" href="#!" data-mdb-ripple="true" data-mdb-ripple-color="light">MENU</a>
+                                </li>
+                                <li className="nav-item">
+                                    <a className="nav-link block pr-2 lg:px-2  lg:py-5 py-2 text-gray-600 hover:text-gray-700 focus:text-gray-700 transition duration-150 ease-in-out" href="#!" data-mdb-ripple="true" data-mdb-ripple-color="light">KAMPANYALAR</a>
                                 </li>
                                 <li className="nav-item mb-2 lg:mb-0">
-                                    <a className="nav-link block pr-2 lg:px-2 py-2 text-gray-600 hover:text-gray-700 focus:text-gray-700 transition duration-150 ease-in-out" href="#!" data-mdb-ripple="true" data-mdb-ripple-color="light">About</a>
+                                    <a className="nav-link block pr-2 lg:px-2 lg:py-5 py-2 text-gray-600 hover:text-gray-700 focus:text-gray-700 transition duration-150 ease-in-out" href="#!" data-mdb-ripple="true" data-mdb-ripple-color="light">KURUMSAL SATIŞ</a>
                                 </li>
                             </ul>
                         </div>
@@ -54,11 +56,7 @@ function Header() {
 
 
 
-                <div className="text-center bg-gray-50 text-gray-800 py-20 px-6">
-                    <h1 className="text-5xl font-bold mt-0 mb-6">Heading</h1>
-                    <h3 className="text-3xl font-bold mb-8">Subeading</h3>
-                    <a className="inline-block px-6 py-2.5 bg-blue-600 text-white font-medium text-xs leading-tight uppercase rounded shadow-md hover:bg-blue-700 hover:shadow-lg focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out" data-mdb-ripple="true" data-mdb-ripple-color="light" href="#!" role="button">Get started</a>
-                </div>
+
 
             </header></>
     )
