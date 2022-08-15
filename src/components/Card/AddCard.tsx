3462cb4dfe3a3ebfa46ba1fe8d9eac25273b54c4
Elif <78614794+elselif@users.noreply.github.com>
2022-08-16 01:12:09 +0300

    typescript-props

--- src/components/Card/AddCard.tsx
+++ src/components/Card/AddCard.tsx
@@ -1,14 +1,23 @@
+import { title } from 'process'
 import React from 'react'
 
-function AddCard() {
+interface Props {
+    title: string,
+    description: string,
+    image : string,
+    buttonText: string,
+}
+
+const AddCard : React.FC<Props>= ({image,title,description,buttonText}) => {
     return (
         <>
             <div className='p-10 inline-flex'>
-                <img src="../../images/homeP2.jpeg" className="w-[390px] h-[350px]" />
+                <img src={image} className="w-[390px] h-[350px]" />
                 <div className='text-left text-2xl font-semibold  pl-5'>
-                    Yazına renk katacak Starbucks® ürünleri Hepsiburada’da!
-                    <p className='text-base font-light mt-6'>Birbirinden farklı renk ve tasarımları ile Starbucks® kupa ve termosları Hepsiburada’da seni bekliyor!</p>
-                    <button type="button" className="mt-4 text-white bg-green-700 hover:text-white border  hover:bg-green-100 focus:ring-4 focus:outline-none focus:ring-gray-300 font-medium rounded-full text-sm px-5 py-1.5 text-center mr-2 mb-0 ">Hemen Katıl</button>
+                    {title}
+                    
+                    <p className='text-base font-light mt-6'>{description}</p>
+                    <button type="button" className="mt-4 text-white bg-green-700 hover:text-white border  hover:bg-green-100 focus:ring-4 focus:outline-none focus:ring-gray-300 font-medium rounded-full text-sm px-5 py-1.5 text-center mr-2 mb-0 ">{buttonText}</button>
 
                 </div>
             </div>
