5684b4561b0c09a4e95ee3a3fecc1b13965a8abd
Elif <78614794+elselif@users.noreply.github.com>
2022-08-25 17:43:04 +0300

    -

--- src/Layouts/Sticky.tsx
+++ src/Layouts/Sticky.tsx
@@ -6,8 +6,8 @@ export default function Sticky() {
   const hour = date.getHours();
 
   return (
-    <div className="basis-2/5"  >
-      <div className="w-[578px] h-full border-r-2 shadow-xl mix-blend-multiply  relative " >
+    <div className="basis-2/5 "  >
+      <div className="w-[578px] h-[100%] border-r-2 shadow-xl mix-blend-multiply  relative " >
         <img src="../../images/mainleft.jpeg" className="w-[350px] h-[615px] items-center mx-auto pt-10 mb-10 " />
         <div className="text-4xl w-[400px] p-8 pt-3">
           {
